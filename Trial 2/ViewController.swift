b87e50896e2509450862a37f941755eff7a43733
Gerardo Jesus Arguello Haces <gerardo@gerardos-mbp.lan>
2017-11-21 13:41:34 -0600

    Initial Commit

---
+++ Trial 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trial 2
+//
+//  Created by Gerardo Jesus Arguello Haces on 11/21/17.
+//  Copyright © 2017 BakFood Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
